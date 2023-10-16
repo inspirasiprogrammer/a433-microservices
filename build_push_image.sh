e7d460cb5aaf1d748c6c00d4da04314615a928a9
Irwan Syahputra <irwansyahputra92@gmail.com>
2023-10-16 22:54:03 +0700

    Change to GitHub Packages

--- build_push_image.sh
+++ build_push_image.sh
@@ -3,25 +3,26 @@
 # Nama image yang akan dibuat
 image_name="item-app"
 image_tag="v1"
-dockerhub_username="irsya05"  # Ganti dengan nama pengguna Docker Hub 
+ghcr_username="inspirasiprogrammer"  # Ganti dengan nama pengguna GitHub Anda
+ghcr_repository="a433-microservices"  # Ganti dengan nama repository di GitHub Container Registry Anda
+
 
 # 1. Perintah untuk membuat Docker image dari Dockerfile
 docker build -t $image_name:$image_tag .
-
 # 2. Melihat daftar image di lokal
 docker images
 
-# 3. Mengubah nama image agar sesuai dengan format Docker Hub
-docker tag $image_name:$image_tag $dockerhub_username/$image_name:$image_tag
+# 3. Mengubah nama image agar sesuai dengan format Github Packages
+docker tag $image_name:$image_tag ghcr.io/$ghcr_username/$ghcr_repository/$image_name:$image_tag .
 
-# 4. Login ke Docker Hub
-# Menggunakan perintah 'echo' untuk mengambil nilai dari variabel PASSWORD_DOCKER_HUB dan mengirimkannya sebagai masukan ke perintah 'docker login'.
-# -u $dockerhub_username digunakan untuk mengatur nama pengguna Docker Hub dari variabel 'dockerhub_username'.
+# 4. Login ke Github Packages
+# Menggunakan perintah 'echo' untuk mengambil nilai dari variabel GITHUB_PACKAGES_TOKEN dan mengirimkannya sebagai masukan ke perintah 'docker login'.
+# -u $ghcr_username digunakan untuk mengatur nama pengguna Github Packages dari variabel 'ghcr_username'.
 # --password-stdin menginstruksikan Docker untuk membaca kata sandi dari masukan standar (stdin).
-echo $PASSWORD_DOCKER_HUB | docker login -u $dockerhub_username --password-stdin
+echo $GITHUB_PACKAGES_TOKEN | docker login ghcr.io -u $ghcr_username --password-stdin
 
-# 5. Mengunggah image ke Docker Hub
-docker push $dockerhub_username/$image_name:$image_tag
+# 5. Mengunggah image ke GitHub Container Registry
+docker push ghcr.io/$ghcr_username/$ghcr_repository/$image_name:$image_tag
 
 # 6. Selesai
-echo "Docker image berhasil dibuat."
+echo "Docker image berhasil dibuat dan diunggah ke GitHub Container Registry."
